2adc808e2c5930d90d270221515447ddab848066
Dave Miller <millerd1@wit.edu>
2014-06-11 21:46:21 -0400

    added an image to the navbar and changed some hover highlighting. some bootstrap functionality is not working -- activating links mainly. need to look into this

--- socm/config/application.rb
+++ socm/config/application.rb
@@ -19,5 +19,6 @@ module Socm
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.assets.precompile += %w(bootstrap.css bootstrap-responsive.css bootstrap.js)
+    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
   end
 end
