5aa246f4259cdc594e5d521113159e050fc715ef
Dave Miller <millerd1@wit.edu>
2014-06-18 12:02:18 -0400

    updated width of SOCM main page view. added TODO in view, and placed a small description about what SOCM is and does.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,6 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  #TODO: Uncomment when we merge the rest of the admin changes into master
   #before_action :authenticate_admin!, except: [:index, :show]
 end
