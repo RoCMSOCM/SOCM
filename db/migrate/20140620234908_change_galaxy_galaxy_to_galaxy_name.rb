0107a42c85303dbd9fd7efd19bfc384385813d3c
Dave Miller <millerd1@wit.edu>
2014-06-20 19:58:44 -0400

    emtpy migration for changing galaxy.galaxy to galaxy.galaxy_name and galaxy spec

---
+++ db/migrate/20140620234908_change_galaxy_galaxy_to_galaxy_name.rb
@@ -0,0 +1,4 @@
+class ChangeGalaxyGalaxyToGalaxyName < ActiveRecord::Migration
+  def change
+  end
+end
---
+++ spec/models/galaxy_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+describe Galaxy  do
+  let!(:galaxy) { FactoryGirl.create(:galaxy) }
+
+  it "has a valid Galaxy factory" do
+    galaxy.should be_valid
+  end
+
+  it "does not allow nil values" do
+    FactoryGirl.build(:galaxy, galaxy: nil, galaxy_type: nil, distance: nil, luminosity: nil,
+                      scale_length: nil, mass_hydrogen: nil, mass_disk: nil, stars: nil,
+                      vcr: nil).should_not be_valid
+  end
+
+  it "returns a Galaxy name and TYPE as a string" do
+    galaxy.galaxy.should == "GALAXY NAME"
+    galaxy.galaxy_type.should == "ABC"
+  end
+
+  it "returns a Galaxy with attributes as floats" do
+
+  end
+
+  # TODO: update specs to include future uniqueness of galaxies
+end
