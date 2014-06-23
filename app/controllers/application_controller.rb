d7ad23beeb350734f2eaf02a074948358fd1d2f3
Dave Miller <millerd1@wit.edu>
2014-06-23 11:38:51 -0400

    added set_headers to application controller. trying to allow cross domain requests from ajax

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,14 @@ class ApplicationController < ActionController::Base
 
   #TODO: Uncomment when we merge the rest of the admin changes into master
   #before_action :authenticate_admin!, except: [:index, :show]
+  before_action :set_headers
+
+  private
+    def set_headers
+      headers['Access-Control-Allow-Origin'] = '*'
+      headers['Access-Control-Expose-Headers'] = 'Etag'
+      headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, PATCH, OPTIONS, HEAD'
+      headers['Access-Control-Allow-Headers'] = '*, x-requested-with, Content-Type, If-Modified-Since, If-None-Match'
+      headers['Access-Control-Max-Age'] = '86400'
+    end
 end
