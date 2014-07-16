3adc7c7eaf98be47e081246ce9232b548ccb6795
Dave Miller <millerd1@wit.edu>
2014-07-16 12:45:38 -0400

    added before_save to galaxies to set number of stars based on mass_disk and mass_sun

--- app/models/galaxy.rb
+++ app/models/galaxy.rb
@@ -1,4 +1,7 @@
 class Galaxy < ActiveRecord::Base
+  @@mass_sun = MASS_OF_SUN
+  cattr_reader :mass_sun
+
   has_and_belongs_to_many :citations
   has_many :velocities, dependent: :destroy
 
@@ -10,12 +13,12 @@ class Galaxy < ActiveRecord::Base
   validates :scale_length, presence: true, numericality: {only_float: true}
   validates :mass_hydrogen, presence: true, numericality: {only_float: true}
   validates :mass_disk, presence: true, numericality: {only_float: true}
-  validate :set_stars
+  before_save :set_stars
 
-  #TODO: check M/Msun with James to set number of stars
+  #TODO: check M_disk/Mass_sun with James to set number of stars
   #calculates number of stars based on M/Msun
   def set_stars
-
+    self.stars = self.mass_disk/self.mass_sun
     true
   end
 end
