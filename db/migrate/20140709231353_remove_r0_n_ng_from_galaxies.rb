c6d9f90e7d2eea4cafe7d5b7417e5a7a530768f2
Dave Miller <millerd1@wit.edu>
2014-07-16 13:17:03 -0400

    fixing coluns in galaxy

--- db/migrate/20140709231353_remove_r0_n_ng_from_galaxies.rb
+++ db/migrate/20140709231353_remove_r0_n_ng_from_galaxies.rb
@@ -2,5 +2,6 @@ class RemoveR0NNgFromGalaxies < ActiveRecord::Migration
   def change
     remove_column :galaxies, :n
     remove_column :galaxies, :n_g
+    remove_column :galaxies, :r0
   end
 end
