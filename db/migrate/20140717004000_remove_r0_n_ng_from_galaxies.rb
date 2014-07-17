5cc78d80abe952d015f5a096fd68c4a6e480e84a
Dave Miller <millerd1@wit.edu>
2014-07-17 15:48:16 -0400

    migration issue

--- db/migrate/20140717004000_remove_r0_n_ng_from_galaxies.rb
+++ db/migrate/20140717004000_remove_r0_n_ng_from_galaxies.rb
@@ -1,6 +1,6 @@
 class RemoveR0NNgFromGalaxies < ActiveRecord::Migration
   def change
-    remove_column :galaxies, :r0
+    #remove_column :galaxies, :r0
     remove_column :galaxies, :n
     remove_column :galaxies, :n_g
   end
