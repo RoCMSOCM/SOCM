455bc585f99e57e0791c9df960bc13693e37cd6a
David Miller <david.miller2@staples.com>
2014-07-17 13:17:57 -0400

    testing out citations galaxy reference

--- db/migrate/20140709231353_remove_r0_n_ng_from_galaxies.rb
+++
@@ -1,6 +0,0 @@
-class RemoveR0NNgFromGalaxies < ActiveRecord::Migration
-  def change
-    remove_column :galaxies, :n
-    remove_column :galaxies, :n_g
-  end
-end
--- db/migrate/20140717001916_drop_galaxies_citations.rb
+++
@@ -1,5 +0,0 @@
-class DropGalaxiesCitations < ActiveRecord::Migration
-  def change
-    drop_table :galaxies_citations
-  end
-end
