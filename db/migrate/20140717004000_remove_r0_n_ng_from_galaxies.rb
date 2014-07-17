6bbb49ac423070c001ad99838dccf6554029a80f
Dave Miller <millerd1@wit.edu>
2014-07-17 15:49:40 -0400

    deleted unnessary migration file

--- db/migrate/20140717004000_remove_r0_n_ng_from_galaxies.rb
+++
@@ -1,7 +0,0 @@
-class RemoveR0NNgFromGalaxies < ActiveRecord::Migration
-  def change
-    #remove_column :galaxies, :r0
-    remove_column :galaxies, :n
-    remove_column :galaxies, :n_g
-  end
-end
