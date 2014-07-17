83d0a48d5d49e63aa80f8d6b2e73275d3a080818
Alexandre Clement <alexrdclement@gmail.com>
2014-07-17 14:42:41 -0400

    fffffffffff

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,10 +14,10 @@ galaxy = Galaxy.new(:galaxy_name => "MILKY-WAY", :galaxy_type => "LSB", :distanc
                     :luminosity => 1.6, :scale_length => 2.1, :mass_hydrogen => 0, :mass_disk => 6.43)
 galaxy.save!
 
-galaxy = Galaxy.find(135)
-CSV.foreach(path) do |row|
-	galaxy.velocities.create(:r => row[0], :vrot_data => row[1], :vrot_data_error => row[2])
-end
+# galaxy = Galaxy.find(135)
+# CSV.foreach(path) do |row|
+# 	galaxy.velocities.create(:r => row[0], :vrot_data => row[1], :vrot_data_error => row[2])
+# end
 
 # 100.times do |i|
 #   galaxy = Galaxy.new(:galaxy_name => "Galaxy #{i}", :galaxy_type => "LSB", :distance => (i - 0.0081),
