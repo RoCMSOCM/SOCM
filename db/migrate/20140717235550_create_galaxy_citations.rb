443ff65a76c418c1dca5f1ed3ee620c4f3aa2a2f
Dave Miller <millerd1@wit.edu>
2014-07-17 20:49:38 -0400

    migration not needed in heroku

--- db/migrate/20140717235550_create_galaxy_citations.rb
+++
@@ -1,9 +0,0 @@
-class CreateGalaxyCitations < ActiveRecord::Migration
-  def change
-    create_table :galaxy_citations do |t|
-      t.integer :galaxy_id
-      t.integer :citation_id
-      t.timestamps
-    end
-  end
-end
