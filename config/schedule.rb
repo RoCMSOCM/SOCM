e1d012828a44934407e5ede2cb57a7f44817f418
Dave Miller <millerd1@wit.edu>
2014-07-28 21:27:48 -0400

    changed flag in cron

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,7 +1,7 @@
 #This cron job uses the Whenever gem to ping the site every 59 minutes
 #so SOCM doesn't go idle on heroku's server
-set :environment, 'development'
+set :environment, 'production'
 
 every 10.seconds do
-  puts "ran it at #{Time.now}", environment => "development"
+  puts "ran it at #{Time.now}", environment => "production"
 end
