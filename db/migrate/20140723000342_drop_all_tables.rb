44538c66efa902fb262f3a3f023dd4f12dcf46f1
David Miller <david.miller2@staples.com>
2014-07-29 16:23:37 -0400

    fixed ransack search bug -- again! also added UI changes to welcome controller paragraphs -- class now controls indents. also added missing header for Velocity#show

--- db/migrate/20140723000342_drop_all_tables.rb
+++
@@ -1,9 +0,0 @@
-class DropAllTables < ActiveRecord::Migration
-  def change
-    drop_table :galaxies
-    drop_table :velocities
-    drop_table :citations
-    drop_table :galaxy_citations
-    drop_table :admins
-  end
-end
